d565f5baf011063c30cf839bbf3ec468e6a36f09
Andred17 <Damilaresammy1996@gmail.com>
2025-08-14 19:56:01 +0100

    fixing client url commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const MONGO_URI = process.env.MONGO_URI;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: CLIENT_URL, credentials: true }));
+
 
 // Static folder for images
 app.use("/images", express.static(path.join(__dirname, "upload/images")));
